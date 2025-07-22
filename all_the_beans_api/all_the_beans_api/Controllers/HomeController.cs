9e2ca53430fcfa1c0da1c865aa07d5769fe40e97
Nadi Adem <Nadem@spartaglobal.com>
2025-07-22 16:20:07 +0100

    api changes applied

---
+++ all_the_beans_api/all_the_beans_api/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace all_the_beans_api.Controllers
+{
+    public class HomeController : ControllerBase
+    {
+        
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("GetAllBeans")]
+        public IEnumerable<Beans> Get()
+        {
+            return Ok();
+        }
+    }
+}
